39b50199691e220921a68bddfd17377abe48061a
KusayAlkaleh <theonexr4@gmail.com>
2023-12-13 10:40:23 +0300

    adding Admin and Plane controller

--- WebProject/Controllers/AdminController.cs
+++ WebProject/Controllers/AdminController.cs
@@ -12,11 +12,41 @@ namespace WebProject.Controllers
             this.DemoDbContext = mvcDemoDbContext;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Index()
+		{
+            var users = await DemoDbContext.Users.ToListAsync();
+
+            return View(users);
+		}
+
+		public IActionResult FAQ()
+		{
+			return View();
+		}
+
+		[HttpGet]
         public async Task<IActionResult> ViewUsers()
 		{
 			var users = await DemoDbContext.Users.ToListAsync();
 
 			return View(users);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(int userId)
+		{
+			var deletedUser = await DemoDbContext.Users.FindAsync(userId);
+
+			if (deletedUser != null)
+			{
+				DemoDbContext.Users.Remove(deletedUser);
+				await DemoDbContext.SaveChangesAsync();
+
+				return RedirectToAction("ViewUsers");
+			}
+
+			return RedirectToAction("Index");
 		}
 	}
 }
