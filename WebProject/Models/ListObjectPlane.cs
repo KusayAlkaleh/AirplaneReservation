8282d4abdf2b43cb507b99ab853c5ec31233b5b4
KusayAlkaleh <theonexr4@gmail.com>
2023-12-14 23:23:49 +0300

    adding new model

---
+++ WebProject/Models/ListObjectPlane.cs
@@ -0,0 +1,10 @@
+﻿using WebProject.Models.Domain;
+
+namespace WebProject.Models
+{
+    public class ListObjectPlane
+    {
+        public Plane plane { get; set; }
+        public List<Plane> ListOfPlanes { get; set; }
+    }
+}
