4846166b011c8c44927faddc3add5c6afd72580e
KusayAlkaleh <theonexr4@gmail.com>
2023-12-22 21:47:56 +0300

    Add new Modals to hepl me when i getting data from database

---
+++ WebProject/Models/ModelWithFlight.cs
@@ -0,0 +1,11 @@
+﻿using WebProject.Models.Domain;
+
+namespace WebProject.Models
+{
+    public class ModelWithFlight
+    {
+        public Flight Flight { get; set; }
+        public List<Plane> Planes { get; set; }
+        public List<Airport> Airports { get; set; }
+    }
+}
