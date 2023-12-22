62458db564df97d0d3f98d34443b99c54682301d
KusayAlkaleh <theonexr4@gmail.com>
2023-12-22 21:46:12 +0300

    Edit some fields in AirportController

---
+++ WebProject/Controllers/AirportController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Data;
+using WebProject.Models;
+using WebProject.Models.Domain;
+
+namespace WebProject.Controllers
+{
+    public class AirportController : Controller
+    {
+        private readonly DemoDbContext DemoDbContext;
+        public AirportController(DemoDbContext mvcDemoDbContext)
+        {
+            this.DemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            List<Airport> allAirports = await DemoDbContext.Airports.ToListAsync();
+            if(allAirports != null)
+            {
+                ListWithAirport listWithAirport = new ListWithAirport
+                {
+                    Airports = allAirports
+                };
+                return View(listWithAirport);
+            }
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ListWithAirport model)
+        {
+            if(model.Airport != null)
+            {
+                await DemoDbContext.Airports.AddAsync(model.Airport);
+                await DemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Create");
+            }
+            return RedirectToAction("Index", "Admin");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ListWithAirport model)
+        {
+            if (model != null && model.Airport != null)
+            {
+                DemoDbContext.Airports.Update(model.Airport);
+                await DemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Create");
+            }
+
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public async Task<IActionResult> Delete(int airportId)
+        {
+            var airportInformation = await DemoDbContext.Airports.FindAsync(airportId);
+
+            if(airportInformation != null)
+            {
+                DemoDbContext.Airports.Remove(airportInformation);
+                await DemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Create");
+            }
+
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
