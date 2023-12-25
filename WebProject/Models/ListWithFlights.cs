bd01bde33b94fb3e9d9593aec92f39c5cdf4ea82
KusayAlkaleh <theonexr4@gmail.com>
2023-12-25 19:19:47 +0300

    Add new model with contains three types

---
+++ WebProject/Models/ListWithFlights.cs
@@ -0,0 +1,11 @@
+﻿using WebProject.Models.Domain;
+
+namespace WebProject.Models
+{
+    public class ListWithFlights
+    {
+        public Flight Flight { get; set; }
+        public List<Flight> Flights { get; set; }
+        public Dictionary<int, string> AirportNames { get; set; }
+    }
+}
