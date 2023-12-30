1f98336eb094dd821e6a2653f0bd0c62b992f7fb
KusayAlkaleh <theonexr4@gmail.com>
2023-12-30 23:27:25 +0300

    General Identity strucuture

---
+++ WebProject/Repositories/Implementation/UserAuthenticationService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebProject.Repositories.Implementation
+{
+    public class UserAuthenticationService
+    {
+        // Constructs for Identity
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UserAuthenticationService(SignInManager<ApplicationUser> signInManager,
+                                         UserManager<ApplicationUser> userManager,
+                                         RoleManager<IdentityRole> roleManager)
+        {
+            this.signInManager = signInManager;
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+    }
+}
