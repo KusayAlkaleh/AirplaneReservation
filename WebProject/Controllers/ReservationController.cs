599ffd6a53da9a01a36ef11b20b35ae6afe6e0b6
KusayAlkaleh <theonexr4@gmail.com>
2024-01-01 22:36:41 +0300

    Add Reservation Model and it views!

--- WebProject/Controllers/ReservationController.cs
+++ WebProject/Controllers/ReservationController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Data;
 
 namespace WebProject.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly DemoDbContext DemoDbContext;
+        public ReservationController(DemoDbContext mvcDemoDbContext)
         {
-            return View();
+            this.DemoDbContext = mvcDemoDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var reservationsInformation = await DemoDbContext.Reservation.ToListAsync();
+
+            if( reservationsInformation == null )
+            {
+                return NotFound();
+            }
+
+            return View(reservationsInformation);
         }
     }
 }
